435a8b69c06ffc4d3dfce93bb52756f81c8796cc
hugosenari <hugosenari@gmail.com>
2022-03-22 00:38:03 -0300

    chore: re-index ../nimpkgs/s/statsd_client

--- 0_1_1/flake.nix
+++ 0_1_1/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''A simple, stateless StatsD client library'';
+  description = ''statsd client'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-statsd_client-0_1_1.flake = false;
   inputs.src-statsd_client-0_1_1.owner = "FedericoCeratto";
-  inputs.src-statsd_client-0_1_1.ref   = "refs/tags/0.1.1";
+  inputs.src-statsd_client-0_1_1.ref   = "0_1_1";
   inputs.src-statsd_client-0_1_1.repo  = "nim-statsd-client";
   inputs.src-statsd_client-0_1_1.type  = "github";
   
